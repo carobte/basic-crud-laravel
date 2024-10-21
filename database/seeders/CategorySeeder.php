717dca3f777d4aa7eb853693776d83335ccb2212
carobte <caro.bustamante.escobar@gmail.com>
2024-10-21 09:24:10 -0500

    feat: seeders

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            [
+                'name' => 'electronics',
+                'description' => 'devices and gadgets'
+            ],
+            [
+                'name' => 'books',
+                'description' => 'books and literature'
+            ],
+            [
+                'name' => 'clothing',
+                'description' => 'apparel and accessories'
+            ]
+        ];
+
+        foreach ($categories as $category) {
+            Category::firstOrCreate(
+                ['name' => $category['name']],
+                ['description' => $category['description']]
+            );
+        }
+    }
+}
